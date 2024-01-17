6ef9ed41e313ef5761eaec0cdf118475d0c03e37
Wanisleia Norato <leianorato@gmail.com>
2024-01-17 19:44:36 -0300

    First Class: Posicao

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,8 +1,12 @@
 package application;
 
+import tabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		System.out.println("Olá");
+
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
 	}
 }
